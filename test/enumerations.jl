5a8323975238190f8a82b38d0abde1217082dbec
Klaus Crusius <klaus.crusius@web.de>
2019-09-17 17:50:45 +0200

    added tests

--- test/enumerations.jl
+++ test/enumerations.jl
@@ -1,16 +1,20 @@
 
 
+using CommutativeRings: len
+
 @testset "iterate" begin
     @test length(collect(ZZ/7)) == 7
     @test length(collect(GF(3,5))) == 3^5
     @test length(collect(Monic(:x, ZZ/2, 5))) == 2^5
+    @test eltype(Monic(:x, ZZ/2, 5)) == (ZZ/2)[:x]
 end
 
 
-@testset "ofindex $T" for T in (Int, UInt, ZZ{Int8}, ZZ/1000, GF(2,5), QQ{Int})
+@testset "ofindex $T" for (T, r) in ((Int, 0), (UInt, 0), (ZZ{Int8}, 0), (ZZ/77, 77), (GF(2,5),2^5), (QQ{Int}, 0))
     @test iszero(ofindex(0, T))
     @test isone(ofindex(1, T))
     @test ofindex(10, T) isa T
+    @test len(T) == r
 end
 
 @testset "ofindex $T degree $n" for (T, n) in ((GF(2)[:z], 3),)
