2f64a63ecd1f6ea363d19ed736670d9e1315104b
Klaus Crusius <klaus.crusius@web.de>
2019-09-21 17:15:13 +0200

    test file for typevars added

---
+++ test/typevars.jl
@@ -0,0 +1,11 @@
+
+using CommutativeRings: sintern
+
+@testset "sintern" begin
+    @test sintern(:x) === :x
+    @test sintern(12) === 12
+    @test sintern(:x, :y) === :xy
+    @test sintern(big"2") === Symbol(big"2")
+    @test sintern(big"2.0") === Symbol(hash(big"2.0"))
+end
+
