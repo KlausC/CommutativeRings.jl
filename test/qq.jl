acb90a8cd50be15207f86a439b0c8cd1c791e3da
Klaus Crusius <klaus.crusius@web.de>
2019-11-21 14:54:26 +0100

    tests for ratinal gcd

--- test/qq.jl
+++ test/qq.jl
@@ -42,3 +42,11 @@ end
 @testset "arithmetic" begin
     @test QQ(2//3) + QQ(1//2) == QQ(7//6)
 end
+
+@testset "gcd calculations" begin
+    @test gcd(QQ(1//3), QQ(2//5)) == 1 // 15
+    @test gcd(QQ((2*3)//(5*7)), QQ((2*5)//(3*7))) == QQ(2, 3*5*7)
+    @test gcdx(QQ((2*3)//(5*7)), QQ((2*5)//(3*7))) == (QQ(2, 3*5*7), -11, 4)
+    @test lcm(QQ((2*3)//(5*7)), QQ((2*5)//(3*7))) == QQ(2*3*5, 7)
+end
+
