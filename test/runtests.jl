fe53a8af662e02b4f981219ded3140a72ca0f436
Klaus Crusius <klaus.crusius@web.de>
2019-11-26 17:49:32 +0100

    enable all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,11 @@ using Test
 using CommutativeRings
 
 @testset "typevars" begin include("typevars.jl") end
-#=
 @testset "ZZ" begin include("zz.jl") end
 @testset "QQ" begin include("qq.jl") end
 @testset "ZZmod" begin include("zzmod.jl") end
 @testset "univarpolynom" begin include("univarpolynom.jl") end
-=#
 @testset "multivarpolynom" begin include("multivarpolynom.jl") end
-#=
 @testset "fraction" begin include("fraction.jl") end
 @testset "quotient" begin include("quotient.jl") end
 @testset "factorization" begin include("factorization.jl") end
@@ -17,4 +14,3 @@ using CommutativeRings
 @testset "numbertheory" begin include("numbertheoretical.jl") end
 @testset "enumerations" begin include("enumerations.jl") end
 @testset "linearalgebra" begin include("linearalgebra.jl") end
-=#
