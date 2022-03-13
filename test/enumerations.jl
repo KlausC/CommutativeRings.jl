46f469f5c4f756e8f17deb45955ca7ee13fcfef8
KlausC <klausC@users.noreply.github.com>
2022-03-13 22:53:45 +0100

    enum all polynomials

--- test/enumerations.jl
+++ test/enumerations.jl
@@ -35,3 +35,43 @@ end
     @test length(factors(12)) == 6
     @test factors(12) |> collect |> sort == [1, 2, 3, 4, 6, 12]
 end
+
+function fullset(n, k)
+    n == 0 && return [Int[]]
+    res = Vector{Int}[]
+    for s in fullset(n-1, k)
+        for e in -k:k
+            cs = copy(s)
+            push!(cs, e)
+            push!(res, cs)
+        end
+    end
+    Set(res)
+end
+
+using CommutativeRings: select_n_tuple
+
+@testset "enumerate Z^$n - $k" for n = 1:5, k = 1:2
+    @test Set(select_n_tuple.(1:(2k+1)^n, n)) == fullset(n, k)
+end
+
+import CommutativeRings: row2index, index2row, index2indexdegree, indexdegree2index, row2degree, degree2row
+
+@testset "enumerate polynomials" begin
+    @test row2index(0) == 0
+    @test index2row(0) == 0
+    @test index2indexdegree(0) == (0, 0)
+    @test indexdegree2index(0, 0) == 0
+    n = big(10)<<100
+    @test indexdegree2index(index2indexdegree(n)...) == n
+    n = typemax(UInt)
+    @test indexdegree2index(index2indexdegree(n)...) == n
+end
+@testset "enumeration basics type $T" for T in (UInt8, Int8, UInt16, Int16, UInt32, Int32, UInt64, Int64, UInt128, Int128, BigInt)
+    @test row2degree(zero(T)) == 0
+    @test row2degree(one(T)) == 1
+    @test row2degree(T(2)) == 1
+    if T != BigInt
+        @test row2degree(typemax(T)) == row2degree(big(typemax(T)))
+    end
+end
