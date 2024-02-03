764c1c9edccac03132cfc29da225d15607db26b7
KlausC <klausC@users.noreply.github.com>
2024-02-03 15:16:02 +0100

    reduced allocations in convolution

--- src/fourier.jl
+++ src/fourier.jl
@@ -2,6 +2,7 @@
 
 export fft!, fft
 
+
 convolute(a::R, b::R) where R<:Ring = a * b
 
 """
@@ -60,14 +61,14 @@ function fft(n::Integer, f::AbstractVector{R}, w::AbstractVector{R}) where R<:Ri
 end
 
 """
-    fft!(n, F:Vector{R}, dd, z, G::Vector) where R
+    fft!(n, F:Vector{R}, dd, z, W::Vector) where R
 
 Special case of fft over the quotient ring `R[X]/(X^dd + 1)` for `n` and `dd` powers of 2,
 using principal `2dd`th root of unity `ω = X^z, ω^dd == -1`.
 
-Result overwrites input `F`. `G` is workspace of same size as `F`. Assume `n * dd == length(F)`.
+Result overwrites input `F`. `W` is workspace of same size as `F`. Assume `n * dd == length(F)`.
 """
-function fft!(n::Integer, F::V, dd::Int, z::Int, G::V) where {R,V<:AbstractVector{R}}
+function fft!(n::Integer, F::V, dd::Int, z::Int, W::V) where {R,V<:AbstractVector{R}}
     n > 0 && count_ones(n) == 1 || throw(ArgumentError("n must be power of 2"))
     0 < dd && count_ones(dd) == 1 || throw(ArgumentError("dd must be power of 2 < n"))
     length(F) == n * dd || throw(ArgumentError("length(F) must be n * dd"))
@@ -100,12 +101,12 @@ function fft!(n::Integer, F::V, dd::Int, z::Int, G::V) where {R,V<:AbstractVecto
         for j = 0:m:n-1
             for i = 0:k-1
                 for l = 1:dd
-                    G[(2i+j)*dd+l] = F[(i+j)*dd+l]
-                    G[(2i+j+1)*dd+l] = F[(i+k+j)*dd+l]
+                    W[(2i+j)*dd+l] = F[(i+j)*dd+l]
+                    W[(2i+j+1)*dd+l] = F[(i+k+j)*dd+l]
                 end
             end
         end
-        F, G = G, F
+        F, W = W, F
         m += m
     end
     if F !== F0
@@ -154,6 +155,15 @@ function schoenhage_strassen_algo(
     sum(H[i+1] * Y^i for i = 0:δ-1)
 end
 
+"""
+    schoenhage_strassen(F, G, n)
+
+Calculate product of polynomials with coefficients `F` and `G`, both with degree `< n`.
+Return result modulo `x^n + 1`.
+The size of the result vector is reduced to minimum.
+
+Allocates working space of `3 * 2n`.
+"""
 function schoenhage_strassen(F::AbstractVector{R}, G::AbstractVector{R}, n::Int) where R
 
     characteristic(R) == 0 || isunit(R(2)) || throw(ArgumentError("2 has inverse in R"))
@@ -166,29 +176,33 @@ function schoenhage_strassen(F::AbstractVector{R}, G::AbstractVector{R}, n::Int)
     FF = similar(F, 2n)
     GG = similar(G, 2n)
     W = similar(F, 2n)
-    _schoenhage_strassen!(F, G, 0, n, FF, GG, W)
-
+    v, m = schoenhage_strassen!(F, G, n, FF, GG, W)
+    m = something(findlast(!iszero, view(v, 1:m)), 0)
+    resize!(v, m)
 end
 
-function _schoenhage_strassen!(
-    F::Any,
-    G::Any,
-    j::Int,
-    n::Int,
-    FF::Q,
-    GG::Q,
-    W::Q,
-) where Q<:AbstractVector
+"""
+    schoenhage_strassen(F, G, n, FF, GG, W) -> (FF, m)
 
+Like `shoenhage_strassen(F, G, n)` but using preallocated working spaces `FF, GG, W`.
+On return `FF` is keeping the compete result, while `F[1:m]` is the result.
+"""
+function schoenhage_strassen!(F, G, n, FF, GG, W)
     k = ilog2(n)
     d = 2^(k ÷ 2)
     δ = 2^(k - k ÷ 2)
     @assert n == d * δ # = 2^k
+    insert_zeros!(FF, F, 0, n, d)
+    insert_zeros!(GG, G, 0, n, d)
+    _schoenhage_strassen!(k, FF, GG, W)
+end
 
+function _schoenhage_strassen!(k::Int, FF::Q, GG::Q, W::Q) where Q<:AbstractVector
+    d = 2^(k ÷ 2)
+    δ = 2^(k - k ÷ 2)
     z = isodd(k) ? 1 : 2
     w = 2z
-    insert_zeros!(FF, F, j, n, d)
-    insert_zeros!(GG, G, j, n, d)
+
     scale!(FF, 2d, z)
     scale!(GG, 2d, z)
     fft!(δ, FF, 2d, w, W)
@@ -197,7 +211,7 @@ function _schoenhage_strassen!(
     fft!(δ, FF, 2d, -w, W)
     scale!(FF, 2d, -z)
     FF .= sdiv.(FF, δ)
-    shrink!(W, FF, d, δ)
+    shrink!(FF, d, δ)
 end
 
 """
@@ -213,19 +227,26 @@ function scale!(F::AbstractVector, n::Int, z::Int)
     F
 end
 
-function shrink!(A, B, d, δ)
-    length(B) >= 2d * δ || throw(ArgumentError("length of B"))
+"""
+    shrink!(A, d, δ)
+
+Reduce size of `A` from orinally `2*d*δ` to `d*δ` by adding chunks `A[j*2d+1:j*2d+2d]`
+to `A[j*d+1:j*d+2d]` for `j ∈ 0:δ-1`. Special treatment of last `d` to relize
+multiplication modulo `x^(d*δ) + 1`.
+"""
+function shrink!(A, d, δ)
+    length(A) >= 2d * δ || throw(ArgumentError("length of B"))
     length(A) >= d * δ || throw(ArgumentError("length of A"))
     n = d * δ
-    A[1:2d] .= B[1:2d]
-    δ <= 1 && return resize!(A, n)
+    A[1:2d] .= A[1:2d]
+    δ <= 1 && return A, n
     for i = d:d:n-2d
-        A[i+1:i+d] .+= B[2i+1:2i+d]
-        A[i+d+1:i+2d] .= B[2i+d+1:2i+2d]
+        A[i+1:i+d] .+= A[2i+1:2i+d]
+        A[i+d+1:i+2d] .= A[2i+d+1:2i+2d]
     end
-    A[n-d+1:n] .+= B[2n-2d+1:2n-d]
-    A[1:d] .-= B[2n-d+1:2n]
-    resize!(A, n)
+    A[n-d+1:n] .+= A[2n-2d+1:2n-d]
+    A[1:d] .-= A[2n-d+1:2n]
+    return A, n
 end
 
 """
@@ -358,19 +379,25 @@ function convolute(F::AbstractVector{R}, G::AbstractVector{R}) where R
     convolute!(R[], F, G)
 end
 
-function convolute!(H::AbstractVector{R}, F::AbstractVector{R}, G::AbstractVector{R}) where R
+function convolute!(
+    H::AbstractVector{R},
+    F::AbstractVector{R},
+    G::AbstractVector{R},
+) where R
     dF, dG = deg(F), deg(G)
     min(dF, dG) < 0 && return resize!(H, 0)
     N = 2^(ilog2(min(dF, dG)) + 1)
-    resize!(H, max(dF, dG) + N)
+    resize!(H, max(dF, dG) + N + 1)
     m = dF + dG + 1
     convolute_noresize!(H, F, G)
     resize!(H, m)
 end
 
 function convolute_noresize!(H::AbstractVector{R}, F, G) where R
+    dF, dG = deg(F), deg(G)
+    (dF < 0 || dG < 0) && return H
     e1, opt = effort_naive(F, G)
-    e2 = effort_mixed(deg(F) + 1, deg(G) + 1)
+    e2 = effort_mixed(dF + 1, dG + 1)
     if e1 <= e2
         convolute_naive!(H, F, G, opt)
     else
@@ -381,7 +408,7 @@ end
 function convolute_naive!(H, F, G, Fless::Bool)
     dF = deg(F)
     dG = deg(G)
-    println("naive $dF, $dG")
+    @assert !(dF < 0 || dG < 0)
     @assert length(H) >= dF + dG + 1
     fill!(H, 0)
     if Fless
@@ -397,7 +424,7 @@ function convolute_naive!(H, F, G, Fless::Bool)
         for i = 0:dG
             gi = G[i+1]
             if !iszero(gi)
-                for j in 0:dF
+                for j = 0:dF
                     H[j+i+1] += F[j+1] * gi
                 end
             end
@@ -409,33 +436,36 @@ end
 function convolute_mixed!(H, F, G)
     dF, dG = deg(F), deg(G)
     N = 2^(ilog2(min(dF, dG)) + 1)
+    n = 2N
     @assert length(H) >= max(dF, dG) + N
     fill!(H, 0)
+    FF = similar(F, 2n)
+    GG = similar(G, 2n)
+    W = similar(F, 2n)
     if max(dF, dG) + 1 <= N
-        FG = schoenhage_strassen(F, G, 0)
-        n = deg(FG)
-        @assert n < length(H)
-        for j = 1:n+1
+        FG, m = schoenhage_strassen!(F, G, n, FF, GG, W)
+        @assert m <= length(H)
+        for j = 1:m
             H[j] = FG[j]
         end
         return H
     end
     if dF <= dG
-        M = max((dG + 1) ÷ N, 1)
+        M = mod(dG + 1, N)
         convolute_noresize!(H, F, view(G, 1:M))
         for i = M+1:N:dG
-            FG = schoenhage_strassen(F, view(G, i:i-1+N), 0)
-            for j = 0:deg(FG)
-                H[j+i] += FG[j+1]
+            FG, m = schoenhage_strassen!(F, view(G, i:i-1+N), n, FF, GG, W)
+            for j = 1:m
+                H[j+i-1] += FG[j]
             end
         end
     else
         M = mod(dF + 1, N)
         convolute_noresize!(H, view(F, 1:M), G)
         for i = M+1:N:dF
-            FG = schoenhage_strassen(view(F, i:i-1+N), G, 0)
-            for j = 0:deg(FG)
-                H[j+i] += FG[j+1]
+            FG, m = schoenhage_strassen!(view(F, i:i-1+N), G, n, FF, GG, W)
+            for j = 1:m
+                H[j+i-1] += FG[j]
             end
         end
     end
@@ -445,7 +475,7 @@ end
 
 function effort_naive(F, G)
     dF, dG = deg(F), deg(G)
-    (dF < 0 || dG < 0) && return 0
+    (dF < 0 || dG < 0) && return 0, false
     nzF = count(!iszero, F)
     nzG = count(!iszero, G)
     nf = nzF * (dG + 1)
@@ -458,7 +488,7 @@ function effort_mixed(n, m)
     if n <= 1
         max(n * m, 0)
     else
-        N = 2^(ilog2(n-1) + 1)
+        N = 2^(ilog2(n - 1) + 1)
         M = max(m ÷ N, 1)
         effort_sch(N) * M + effort_noresize(max(m - N * M, 0), n)
     end
