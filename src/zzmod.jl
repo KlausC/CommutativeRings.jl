c4735314d48ad86e96187371be8ae02ad49ee2b3
KlausC <klausC@users.noreply.github.com>
2024-02-08 17:08:58 +0100

    @inline zzmod

--- src/zzmod.jl
+++ src/zzmod.jl
@@ -121,7 +121,7 @@ hash(a::ZZmod, h::UInt) = hash(a.val, hash(modulus(a), h))
 dimension(::Type{<:ZZmod}) = 1
 
 # implementation of checked multiplication
-function mult_ZZmod(a::T, b::T, m::T) where T<:Integer
+@inline function mult_ZZmod(a::T, b::T, m::T) where T<:Integer
     p, ov = Base.mul_with_overflow(a, b)
     ov || return mod(p, m)
     T(mod(widemul(a, b), m))
@@ -133,7 +133,7 @@ function mult_ZZmod(a::T1, b::T2, c::T) where {T1<:Integer,T2<:Integer,T<:Intege
 end
 
 # improved version of invmod
-function invmod2(n::T, m::T) where T<:Integer
+@inline function invmod2(n::T, m::T) where T<:Integer
     iszero(m) && throw(DomainError(m, "`m` must not be 0."))
     p = _unsigned(abs(m))
     q = n >= 0 ? rem(_unsigned(n), p) : p - rem(_unsigned(-n), p)
@@ -143,7 +143,7 @@ function invmod2(n::T, m::T) where T<:Integer
     # The postcondition is: mod(widemul(r, n), m) == mod(T(1), m) && div(r, m) == 0
 end
 # assumptions is, that  0 <= n < m
-function invmod_unsigned(n::T, m::T) where T<:Union{Unsigned,BigInt}
+@inline function invmod_unsigned(n::T, m::T) where T<:Union{Unsigned,BigInt}
     n = n < m ? n : rem(n, m)
     g, r, y = gcdx(n, m)
     g != 1 && throw(DomainError((n, m), "Greatest common divisor is $g."))
